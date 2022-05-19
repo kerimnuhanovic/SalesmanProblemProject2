e79ddef459e57af9c3ce230000a159f63c0581cb
kerimnuhanovic <kerimnuhanovic18@gmail.com>
2022-05-20 00:39:46 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/salesmanproblemproject/MainActivity.kt
+++ app/src/main/java/com/example/salesmanproblemproject/MainActivity.kt
@@ -13,5 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         Timber.i("onCreate called!")
+        Timber.i("onCreate called!")
+
     }
 }
