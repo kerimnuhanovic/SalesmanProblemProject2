c6b22c397a4de045a2b15de25e813a923a43f1d4
kerimnuhanovic <kerimnuhanovic18@gmail.com>
2022-05-20 23:43:18 +0200

    Update 20.5

--- app/src/main/java/com/example/salesmanproblemproject/TitleFragment.kt
+++ app/src/main/java/com/example/salesmanproblemproject/TitleFragment.kt
@@ -1,13 +1,17 @@
 package com.example.salesmanproblemproject
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.salesmanproblemproject.databinding.FragmentTitleBinding
+import timber.log.Timber
 
 class TitleFragment : Fragment() {
 
@@ -16,10 +20,23 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-        binding.dugmeSljedeci.setOnClickListener {
+        /*binding.dugmeSljedeci.setOnClickListener {
             it.findNavController().navigate(TitleFragmentDirections.actionTitleFragment2ToListaGradovaFragment())
 
-        }
+        }*/
+        val timer = object: CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                Timber.i("Preostalo" + millisUntilFinished)}
+
+            override fun onFinish() {
+                activity?.findNavController( R.id.myNavHostFragment)?.navigate(R.id.action_titleFragment2_to_listaGradovaFragment2 )
+            }
+        }.start()
+
+        binding.button.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment2_to_listaGradovaFragment2)
+        )
+
         return binding.root;
     }
 
